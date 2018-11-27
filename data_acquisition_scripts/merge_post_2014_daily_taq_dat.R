d14133d225db5f92080616dbb35cc0348487d258
jyzhou95 <41101438+jyzhou95@users.noreply.github.com>
2018-11-26 23:24:34 -0500

    Added script to process raw tick data

--- data_acquisition_scripts/merge_post_2014_daily_taq_dat.R
+++ data_acquisition_scripts/merge_post_2014_daily_taq_dat.R
@@ -1,13 +1,35 @@
 library(data.table)
 
 # Get list of daily quotes files and output aggregate files into one year
-vec_years <- c(2015:2018)
+vec_years <- c(2003:2018)
 for (year in vec_years){
   vec_files <- list.files(paste0("D:/Desktop/tick_data/year_", year))
-  dt.return.this <- rbindlist(lapply(vec_files, function(x){
-    dt.temp <- fread(x)
-    return (dt.temp)
-  }))
+  if (year < 2015){
+    dt.return.this <- fread(paste0("D:/Desktop/tick_data/year_", year, "/", vec_files))
+    dt.return.this <- dt.return.this[,list(dt = paste(DATE, itime), 
+                                           symbol = SYMBOL, 
+                                           bid = BID, 
+                                           ask = OFR,
+                                           bid_size = BIDSIZ,
+                                           ask_size = OFRSIZ)]
+    
+    # Format time
+    dt.return.this[,dt := as.POSIXct(dt, format = "%Y%m%d %H:%M:%OS")]
+    
+  } else{
+    dt.return.this <- rbindlist(lapply(vec_files, function(x){
+      print(x)
+      dt.temp <- fread(paste0("D:/Desktop/tick_data/year_", year, "/", x))
+      dt.temp <- dt.temp[,list(dt = paste(DATE, itime),
+                               symbol = SYM_ROOT, 
+                               bid = BID, 
+                               ask = ASK,
+                               bid_size = BIDSIZ,
+                               ask_size = ASKSIZ)]
+      dt.temp[,dt := as.POSIXct(dt, format = "%Y%m%d %H:%M:%OS")]
+      return (dt.temp)
+    }))
+  }
   # Write file
   write.csv(x = dt.return.this, file = paste0("D:/Desktop/tick_data/year_", year, ".csv"))
 }
