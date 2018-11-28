9b99c77c94d53f95c3dd87e5fb55a783403d7a80
jyzhou95 <41101438+jyzhou95@users.noreply.github.com>
2018-11-28 17:38:57 -0500

    Finished cointegration test

--- statistical_arbitrage/funcFindCointegratedPairs.R
+++ statistical_arbitrage/funcFindCointegratedPairs.R
@@ -20,18 +20,108 @@ funcCheckCointegration <- function(chr.month, dt.stocks, dt.pairs){
   # dt.pairs has the following columns: symbol1, symbol2
   # dt.return.this has the following columns: dt, symbol1, symbol2, coint
   
-  # Create cluster
-  cl<-makeCluster(detectCores(),type="SOCK")
-  # Load relevant libraries into workers
-  clusterEvalQ(cl, library(data.table))
-  clusterEvalQ(cl, library(quantmod))
-  clusterEvalQ(cl, library(tseries))
-  clusterEvalQ(cl, library(timeSeries))
+  # # Create cluster
+  # cl<-makeCluster(detectCores(),type="SOCK")
+  # # Load relevant libraries into workers
+  # clusterEvalQ(cl, library(data.table))
+  # clusterEvalQ(cl, library(quantmod))
+  # clusterEvalQ(cl, library(tseries))
+  # clusterEvalQ(cl, library(timeSeries))
+  # 
+  # dt.return.this.coint <- rbindlist(clusterApply(cl, 1:nrow(dt.pairs), function(x){
+  #   dt.pairs_temp <- dt.pairs[x]
+  #   stock1 <- dt.pairs_temp$symbol1
+  #   stock2 <- dt.pairs_temp$symbol2
+  #   
+  #   dt.stocks_temp <- dt.stocks[symbol %in% c(as.character(stock1), 
+  #                                             as.character(stock2))]
+  #   
+  #   # Check to make sure that the number of rows is the same for both symbols
+  #   dt.check <- dt.stocks_temp[,.N, by = list(symbol)]
+  #   if (dt.check[1]$N != dt.check[2]$N){
+  #     # Only keep rows where data exists for both symbols
+  #     vec_dates <- intersect(dt.stocks_temp[symbol == stock1]$dt,
+  #                            dt.stocks_temp[symbol == stock2]$dt)
+  #     dt.stocks_temp <- dt.stocks_temp[dt %in% vec_dates]
+  #   }
+  #   
+  #   flt.correlation <- cor(dt.stocks_temp[symbol == stock1]$price, 
+  #                          dt.stocks_temp[symbol == stock2]$price, 
+  #                          method = "pearson")
+  #   
+  #   # dt.plot_temp <- dt.stocks_temp
+  #   # dt.plot_temp[,id := rep(1:(nrow(dt.plot_temp)/2),2)]
+  #   # 
+  #   # ggplot(cbind(dt.stocks_temp[symbol == stock1][,list(stock1_price = price)],
+  #   #              dt.stocks_temp[symbol == stock2][,list(stock2_price = price)]),
+  #   #        aes(x = stock1_price, y = stock2_price)) + geom_point() + theme_bw(base_size = 15)
+  # 
+  #   
+  #   if (flt.correlation >= 0.9){
+  #     # Do a linear regression fit to find the residual
+  #     comb1 = lm(dt.stocks_temp[symbol == stock1]$price ~ dt.stocks_temp[symbol == stock2]$price)
+  #     comb2 = lm(dt.stocks_temp[symbol == stock2]$price ~ dt.stocks_temp[symbol == stock1]$price)
+  #     
+  #     # Find lowest ADF test value
+  #     comb1_adf <- adf.test(comb1$residuals, k=1)
+  #     comb2_adf <- adf.test(comb2$residuals, k=1)
+  #     
+  #     # Use the ADF test with the lowest ADF value
+  #     if (comb1_adf$statistic < comb2_adf$statistic){
+  #       adf.test.p.value <- as.numeric(comb1_adf$p.value)
+  #       adf.test.value <- as.numeric(comb1_adf$statistic)
+  #       # Get standard error 
+  #       residual <- summary(comb1)$sigma
+  #       beta <- as.numeric(comb1$coefficients[2])
+  #     } else{
+  #       adf.test.p.value <- as.numeric(comb2_adf$p.value)
+  #       adf.test.value <- as.numeric(comb2_adf$statistic)
+  #       # Get standard error 
+  #       residual <- summary(comb2)$sigma
+  #       beta <- as.numeric(comb2$coefficients[2])
+  #     }
+  #     
+  #     if (adf.test.p.value < 0.05){
+  #       return (data.table(dt = chr.month,
+  #                          symbol1 = stock1,
+  #                          symbol2 = stock2,
+  #                          coint = 1,
+  #                          p_value = adf.test.p.value,
+  #                          test_value = adf.test.value,
+  #                          residual_se = residual,
+  #                          beta = beta
+  #       ))
+  #     } else{
+  #       return (data.table(dt = chr.month,
+  #                          symbol1 = stock1,
+  #                          symbol2 = stock2,
+  #                          coint = 0,
+  #                          p_value = NA,
+  #                          test_value = NA,
+  #                          residual_se = NA,
+  #                          beta = NA
+  #       ))
+  #     }
+  #   } else{
+  #     return (data.table(dt = chr.month,
+  #                        symbol1 = stock1,
+  #                        symbol2 = stock2,
+  #                        coint = 0,
+  #                        p_value = NA,
+  #                        test_value = NA,
+  #                        residual_se = NA,
+  #                        beta = NA))
+  #   }
+  # }))
+  # stopCluster(cl)
   
-  dt.return.this.coint <- rbindlist(clusterApply(cl, 1:nrow(dt.pairs), function(x){
+  dt.return.this.coint <- rbindlist(lapply(1:nrow(dt.pairs), function(x){
     dt.pairs_temp <- dt.pairs[x]
     stock1 <- dt.pairs_temp$symbol1
     stock2 <- dt.pairs_temp$symbol2
+    
+    # print(glue("Testing pair: {stock1} and {stock2}"))
+    # print(glue("Progress: {round(x/nrow(dt.pairs) * 100, 2)}%"))
     
     dt.stocks_temp <- dt.stocks[symbol %in% c(as.character(stock1), 
                                               as.character(stock2))]
@@ -113,7 +203,7 @@ funcCheckCointegration <- function(chr.month, dt.stocks, dt.pairs){
                          beta = NA))
     }
   }))
-  stopCluster(cl)
+  
   return (dt.return.this.coint)
 }
 
@@ -133,7 +223,7 @@ dt.stock_pairs <- funcGetAllPossiblePairs(vec.stock_list)
 
 
 # Read and process data here
-dt.final_pairs <- rbindlist(lapply(c(2003:2018)), function(x){
+dt.final_pairs <- rbindlist(lapply(c(2003:2018), function(x){
   dt.data <- fread(paste0("D:/Desktop/tick_data/year_", x, ".csv"))
   
   # Remove rows without liquidity
@@ -153,13 +243,15 @@ dt.final_pairs <- rbindlist(lapply(c(2003:2018)), function(x){
   vec.end_months <- vec.start_months + months(1) - 1
   
   dt.return.this <- rbindlist(lapply(1:length(vec.start_months), function(y){
+    print(glue("{vec.start_months[y]}"))
     dt.temp <- dt.data_final[dt >= vec.start_months[y] & dt <= vec.end_months[y]]
     dt.final_temp <- funcCheckCointegration(chr.month = vec.start_months[y],
                                             dt.stocks = dt.temp,
                                             dt.pairs = dt.stock_pairs)
+    return (dt.final_temp)
   }))
-  
-})
+  return (dt.return.this)
+}))
 
 
 
